0bcf03d46c659fd4501bdcbb72633e89eaf0a6b3
shashi8544 <shashi19092003ranjan@gmail.com>
2023-10-18 05:04:41 +0530

    Updates

---
+++ precache-manifest.300cf1ecd5c2a27162d3536b91aa09ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456588b8c71153b28ad6990954616202",
+    "url": "/Quicksell_Assignment/index.html"
+  },
+  {
+    "revision": "9d0bd880be886326dfeb",
+    "url": "/Quicksell_Assignment/static/css/main.e3f8bd2e.chunk.css"
+  },
+  {
+    "revision": "eb05e992b6077396bb5a",
+    "url": "/Quicksell_Assignment/static/js/2.551f202f.chunk.js"
+  },
+  {
+    "revision": "53e5de17b01785e42125",
+    "url": "/Quicksell_Assignment/static/js/3.6c5d1d50.chunk.js"
+  },
+  {
+    "revision": "9d0bd880be886326dfeb",
+    "url": "/Quicksell_Assignment/static/js/main.08a10cbc.chunk.js"
+  },
+  {
+    "revision": "47abfbf83a549cff6693",
+    "url": "/Quicksell_Assignment/static/js/runtime~main.e5ea59b2.js"
+  }
+]);
